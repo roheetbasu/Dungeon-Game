a692e00b15314e59976f32a496b58a2894f48453
roheetbasu <158186264+roheetbasu@users.noreply.github.com>
2024-02-10 18:39:37 +0545

    Update dungeon game.cpp

--- dungeon game.cpp
+++ dungeon game.cpp
@@ -82,7 +82,7 @@ int main() {
     int r, c;
     cout << "Enter the size for mage(matrix m*n):";
     cin >> r >> c;
-    int* mat = new int[r * c];
+    int* mat = new int[r * c]; 
     cout << "Enter the value for dungeon rooms:" << endl;
     for (int i = 0; i < r; i++)
     {
@@ -98,13 +98,4 @@ int main() {
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
